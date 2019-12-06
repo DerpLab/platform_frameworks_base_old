koen01/frameworks_base

Partial screenshot: allow BACK action only from right side
so we can draw our partial screenshot rectangle swiping from the left
side of the screen

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -85,6 +85,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
+import com.android.internal.util.custom.Utils;
 import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.SystemUI;
@@ -675,6 +676,7 @@ class GlobalScreenshot {
         mPartialShot = true;
         ViewConfiguration vc = ViewConfiguration.get(mContext);
         final int touchSlop = vc.getScaledTouchSlop();
+        Utils.setPartialScreenshot(true);
         mScreenshotSelectorView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -693,6 +695,7 @@ class GlobalScreenshot {
                         float x = event.getRawX();
                         float y = event.getRawY();
                         view.setVisibility(View.GONE);
+                        Utils.setPartialScreenshot(false);
                         mWindowManager.removeView(mScreenshotLayout);
                         if (Math.abs(mTouchDownX - x) > touchSlop ||
                             Math.abs(mTouchDownY - y) > touchSlop) {
@@ -738,6 +741,8 @@ class GlobalScreenshot {
             mWindowManager.removeView(mScreenshotLayout);
             mScreenshotSelectorView.stopSelection();
         }
+        // called from action_cancel and also when unbinding screenshot service
+        Utils.setPartialScreenshot(false);
     }
 
     /**

DerpLab/platform_frameworks_base_old

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -85,6 +85,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
+import com.android.internal.util.aosip.aosipUtils;
 import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.SystemUI;
@@ -675,6 +676,7 @@ class GlobalScreenshot {
         mPartialShot = true;
         ViewConfiguration vc = ViewConfiguration.get(mContext);
         final int touchSlop = vc.getScaledTouchSlop();
+        aosipUtils.setPartialScreenshot(true);
         mScreenshotSelectorView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -693,6 +695,7 @@ class GlobalScreenshot {
                         float x = event.getRawX();
                         float y = event.getRawY();
                         view.setVisibility(View.GONE);
+                        aosipUtils.setPartialScreenshot(false);
                         mWindowManager.removeView(mScreenshotLayout);
                         if (Math.abs(mTouchDownX - x) > touchSlop ||
                             Math.abs(mTouchDownY - y) > touchSlop) {
@@ -738,6 +741,8 @@ class GlobalScreenshot {
             mWindowManager.removeView(mScreenshotLayout);
             mScreenshotSelectorView.stopSelection();
         }
+        // called from action_cancel and also when unbinding screenshot service
+        aosipUtils.setPartialScreenshot(false);
     }
 
     /**